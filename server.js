f0394b61b46c24bb560da9ff57a40ab742bc550a
Dan Peters <daniel.r.peters@icloud.com>
2016-11-27 18:04:18 -0800

    Make the server a module

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const http = require('http')
+const url = require('url')
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type":"text/plain"})
-  response.write("Hello, World")
-  response.end()
-}).listen(3000)
+const PORT = 3000
+
+function start () {
+  function onRequest (request, response) {
+    console.log('request received on port:', PORT) // it calls this twice, weird
+    response.writeHead(200, {"Content-Type":"text/plain"})
+    response.write("Hello, World")
+    response.end()
+  }
+
+  http.createServer(onRequest).listen(PORT)
+
+  console.log('server started on port:', PORT)
+
+  // side note
+  function say (string) {
+    console.log(string)
+  }
+
+  function sayString (say, string) {
+    say(string)
+  }
+
+  sayString(say, 'Hello')
+}
+
+module.exports = start
